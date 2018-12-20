d7ae3727df8a77572d974234a63b17dc73cf8e32
Jiuqing Song <jisong@microsoft.com>
2018-12-20 11:11:06 -0800

    Reformat code

--- tools/prettier.js
+++ tools/prettier.js
@@ -4,9 +4,14 @@ var exec = require('child_process').execSync;
 function run(target) {
     var options = {
         stdio: 'inherit',
-        cwd: path.join(__dirname, '..')
+        cwd: path.join(__dirname, '..'),
     };
-    exec(`node node_modules/prettier/bin-prettier --tab-width 4 --print-width 100 --write --trailing-comma es5 --jsx-bracket-same-line --single-quote "` + target + '"', options);
+    exec(
+        `node node_modules/prettier/bin-prettier --tab-width 4 --print-width 100 --write --trailing-comma es5 --jsx-bracket-same-line --single-quote --end-of-line crlf "` +
+            target +
+            '"',
+        options,
+    );
 }
 
 run('packages/**/*.ts');
